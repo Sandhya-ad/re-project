ddeb0d446f03418a136ed7d03a308162c9d53b05
Sandhya-ad <adhikar2@ualberta.ca>
2024-10-22 23:22:17 -0600

    Very basic event and user class with just getter and setters.

---
+++ Coffee2_app/app/src/main/java/com/example/coffee2_app/Event.java
@@ -0,0 +1,77 @@
+package com.example.coffee2_app;
+
+import android.content.Intent;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Event implements Serializable {
+    private String id;
+    private String name;
+    private String organizer;
+    private String description;
+    private int maxAttendees;
+    private List<User> attendees;
+    private List<User> waitingList;
+    private String hashQrData;
+    private String startDate;
+    private String endDate;
+
+    //Constructor if no maxAttendees
+    public Event(String id, String name, String organizer,String startDate, String endDate) {
+        this.id = id;
+        this.name = name;
+        this.attendees = new ArrayList<>();
+        this.waitingList = new ArrayList<>();
+        this.organizer = organizer;
+        this.startDate= startDate;
+        this.endDate = endDate;
+    }
+
+
+    // Constructor if maxAttendees
+    public Event(String id, String name, String organizer, String startDate, String endDate, Integer maxAttendees) {
+        this.id = id;
+        this.name = name;
+        this.attendees = new ArrayList<>();
+        this.waitingList = new ArrayList<>();
+        this.organizer = organizer;
+        this.startDate= startDate;
+        this.endDate = endDate;
+        this.maxAttendees = maxAttendees;
+    }
+
+    // Getter methods (accessible to all)
+    public String getId() {
+        return id;
+    }
+
+    public String getHashQrData(){
+        return hashQrData;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getOrganizer(){
+        return organizer;
+    }
+    public int getMaxAttendees() {
+        return maxAttendees;
+    }
+
+    public String getStartDate(){
+        return startDate;
+    }
+
+    public String getEndDate(){
+        return endDate;
+    }
+    public List<User> getAttendees() {
+        return attendees;
+    }
+    public List<User> getWaitingList() {
+        return waitingList;
+    }
+}
