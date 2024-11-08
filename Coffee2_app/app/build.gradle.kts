a65d180568ea7663a2c63becc2cb74cf8cc46175
Sandhya-ad <adhikar2@ualberta.ca>
2024-11-08 11:23:28 -0700

    Dependency issue fixed.

--- Coffee2_app/app/build.gradle.kts
+++ Coffee2_app/app/build.gradle.kts
@@ -1,8 +1,16 @@
 configurations {
     all {
-        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib")
-        exclude(group = "org.jetbrains.kotlin", module = "kotlin-coroutines-core")
-        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
+//        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib")
+//        exclude(group = "org.jetbrains.kotlin", module = "kotlin-coroutines-core")
+//        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
+    }
+    configurations.all {
+        resolutionStrategy {
+            force("com.google.firebase:firebase-common:20.4.2")
+            force("com.google.android.gms:play-services-basement:18.4.0")
+            force("com.google.android.gms:play-services-tasks:18.2.0")
+            force("com.google.firebase:firebase-annotations:16.2.0")
+        }
     }
 }
 
@@ -47,14 +55,19 @@ android {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.0")
     //check later for the implementation of firebase
     implementation ("androidx.recyclerview:recyclerview:1.2.1")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
     implementation("com.google.firebase:firebase-firestore")
-    implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("com.google.firebase:firebase-storage")
-    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("com.google.firebase:firebase-auth") {
+        exclude(group = "com.google.firebase", module = "firebase-common")
+    }
+
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
@@ -63,7 +76,7 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.espresso.intents)
-    implementation(libs.firebase.storage)
+    //implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     testImplementation(libs.androidx.junit)
     testImplementation(libs.espresso.core)
